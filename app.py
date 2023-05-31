26f728af98ace9b2d8cd3dda7e35671dcd0d9b9f
math-lover31415 <pradyumn3.1415@gmail.com>
2023-05-31 21:09:17 +0530

    bootstrap

--- app.py
+++ app.py
@@ -4,28 +4,45 @@ from command_line import bulls_and_cows, random_key
 
 app=Flask(__name__)
 
+n=0
 key=random_key()
 answers=[]
+num_list=[]
+win_status=False
 
 @app.route('/')
 @app.route('/game')
 def game():
-    return render_template("page.html",answers=answers)
+    return render_template("page.html",answers=answers, n=n, win_status=win_status)
 
 @app.route('/add', methods=["POST"])
 def add():
+    global win_status, n
+    n+=1
     dictionary={}
     form=request.form
-    dictionary["number"]=form["number"]
-    bulls,cows = bulls_and_cows(dictionary["number"],key)
-    dictionary["bulls"] = bulls
-    dictionary["cows"] = cows
-    answers.append(dictionary)
+    number=form["number"]
+    if number==key:
+        win_status=True
+    if number and (number not in num_list):
+        num_list.append(number)
+        dictionary["number"] = number
+        bulls,cows = bulls_and_cows(number,key)
+        dictionary["bulls"] = bulls
+        dictionary["cows"] = cows
+        answers.append(dictionary)
     return redirect(url_for("game"))
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 @app.route("/new_game", methods=["GET"])
 def new_game():
-    global key, answers
+    global key, answers, num_list, n, win_status
+    n=0
+    win_status=False
+    num_list=[]
     key=random_key()
     answers=[]    
     return redirect(url_for('game'))
