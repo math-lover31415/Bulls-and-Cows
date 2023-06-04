017eb7d29b0c44447ec531992e7af7954cce159c
math-lover31415 <pradyumn3.1415@gmail.com>
2023-06-04 13:30:27 +0530

    Fixed bug
    The new game button was resetting the game globally. Fixed that

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ app=Flask(__name__)
 app.secret_key = 'DA-9A-C9-9D-AB-65'
 
 def reset():
-    global key, answers, num_list, n, win_status
-    n=0
-    win_status=False
-    num_list=[]
-    key=random_key()
-    answers=[]
+    session['n'] = 0
+    session['win_status'] = False
+    session['num_list'] = []
+    session['key'] = random_key()
+    session['answers'] = []
 
 @app.before_request
 def before_request():
@@ -23,24 +22,25 @@ def before_request():
 def game():
     if 'visited' not in session:
         session['visited'] = True
-    return render_template("page.html",answers=answers, n=n, win_status=win_status)
+    return render_template("page.html",answers=session.get('answers', []), n=session.get('n', 0), win_status=session.get('win_status', False), key=session['key'])
 
 @app.route('/add', methods=["POST"])
 def add():
-    global win_status, n
-    n+=1
-    dictionary={}
-    form=request.form
-    number=form["number"]
-    if number==key:
-        win_status=True
-    if number and (number not in num_list):
-        num_list.append(number)
+    session['n'] = session.get('n', 0)+1
+    dictionary = {}
+    form = request.form
+    number = form["number"]
+    key = session.get('key', random_key())
+    session['key'] = key
+    if number == key:
+        session['win_status'] = True
+    if number and (number not in session.get('num_list', [])):
+        session['num_list'] = session.get('num_list', []) + [number]
         dictionary["number"] = number
         bulls,cows = bulls_and_cows(number,key)
         dictionary["bulls"] = bulls
         dictionary["cows"] = cows
-        answers.append(dictionary)
+        session['answers'] = session.get('answers', []) + [dictionary]
     return redirect(url_for("game"))
 
 @app.route('/about')
